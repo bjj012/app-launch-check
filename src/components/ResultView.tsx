c15374ccb1b9a80d4e00f59dbefbba722c4657c8
Alano <3373813130@qq.com>
2026-01-01 11:03:49 +0800

    Add use client directive

--- src/components/ResultView.tsx
+++ src/components/ResultView.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { AuditResult, Issue } from '../lib/types';
 
--- src/components/StepWizard.tsx
+++ src/components/StepWizard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react';
 import { FormData } from '../lib/types';
 
